dad5418bedf024f3ec8b464fa079f2b3ec9ee105
lari5685 <31612106+lari5685@users.noreply.github.com>
2019-09-19 18:08:23 +0700

    Update test_8.py

--- test_8.py
+++ test_8.py
@@ -1,6 +1,6 @@
-max = 11**6
+max = 10**6+1
 min = 0
-n_m	= 0
+n_m = 0
 
 def number(n, num):
 	if n > num:
@@ -17,23 +17,24 @@ for i in range(max):
 	while True:
 		n += 1
 
-		num = min + (max - min )//2
+		num = min + (max - min)//2
 
 		#print('? '+ str(num))
 
-		inp = number(num, i)
+		inp = number(num, i)#int(input())
 
 		if inp == '=':
 			#print('! ', str(num))
 			break
 
 		if inp == '>':
-			min = num + 1
+			min = num
 
 		elif inp == '<':
-			max = num - 1
+			max = num
 
 	if n > n_m:
 		n_m = n
 	#print(str(i)+' '+str(n))
 print(n_m)
+input()
