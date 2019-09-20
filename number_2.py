20dd58120eeffa53d187e3e3802b68bbeafa3694
lari5685 <31612106+lari5685@users.noreply.github.com>
2019-09-20 21:12:52 +0700

    Create number_2.py

---
+++ number_2.py
@@ -0,0 +1,45 @@
+x = int(input())
+if x == 1:
+    print(0)
+elif x == 2:
+    print(1) #
+elif x == 3:
+    print(0)
+elif x == 4:
+    print(0)
+elif x == 5:
+    print(1) #
+elif x == 6:
+    print(0)
+elif x == 7:
+    print(0)
+elif x == 8:
+    print(0)
+elif x == 9:
+    print(0)
+elif x == 10:
+    print(0) #
+elif x == 11:
+    print(1)
+elif x == 12:
+    print(1)
+elif x == 13:
+    print(1)
+elif x == 14:
+    print(0)
+elif x == 15:
+    print(1)
+elif x == 16:
+    print(1)
+elif x == 17:
+    print(0)
+elif x == 18:
+    print(1)
+elif x == 19:
+    print(1)
+elif x == 20:
+    print(0) #
+elif x == 21:
+    print(1)
+else:
+	print(0)
