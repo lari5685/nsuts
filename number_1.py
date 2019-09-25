2306d80746e13bee6d4ab4c402678c1a726fadc1
lari5685 <31612106+lari5685@users.noreply.github.com>
2019-09-25 20:35:44 +0700

    Update number_1.py

--- number_1.py
+++ number_1.py
@@ -1,10 +1,9 @@
-i = int(input())
-m = abs(i)
-if i%2 == 0:
-	h = (m // 2) * (m + 1)
-else:
-	h = m * ((m + 1) // 2)
-		
-if i <= 0:
-	h = -1 * h
-print(h)
+from math import copysign
+
+n = int(input())
+
+am  = abs(1-n)+1
+d   = int(copysign(1.0, n)) if n != 0 else -1
+ans = (2+d*(am-1))*am
+
+print(int(ans)//2)
